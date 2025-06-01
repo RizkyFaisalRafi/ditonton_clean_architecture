eaefbeca5ca91bba113fdb4d898d7c3766754895
Rizky Faisal Rafi <87520408+RizkyFaisalRafi@users.noreply.github.com>
2025-06-02 01:00:15 +0700

    Move File

--- test/presentation/pages/tv_series/on_the_air_page_test.dart
+++ test/presentation/pages/tv_series/on_the_air_page_test.dart
@@ -7,7 +7,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 import 'package:provider/provider.dart';
-import '../../pages/tv_series/tv_series_page_test.mocks.dart';
+import 'tv_series_page_test.mocks.dart';
 
 @GenerateMocks([TvListNotifier])
 void main() {
