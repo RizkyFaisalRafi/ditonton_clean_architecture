6b970ef604ee795160931f1ebbf7f541c8e2e4c7
Rizky Faisal Rafi <87520408+RizkyFaisalRafi@users.noreply.github.com>
2025-06-14 20:00:13 +0700

    Tidy Up

--- lib/presentation/bloc/tv/tv_list/bloc/on_the_air_tv_bloc.dart
+++ lib/presentation/bloc/tv/tv_list/bloc/on_the_air_tv_bloc.dart
@@ -16,13 +16,9 @@ class OnTheAirTvBloc extends Bloc<OnTheAirTvEvent, OnTheAirTvState> {
 
   OnTheAirTvBloc({required this.getOnTheAirTv}) : super(Initial()) {
     // Fetch untuk pertama kali
-
     on<FetchInitialOnTheAir>(_onFetchInitialOnTheAir);
-
     // For Refresh
-
     on<RefreshTvOTA>(_onRefreshTvOTA);
-
     // Gunakan `droppable()` untuk mencegah request ganda saat user scroll cepat
     on<FetchMoreOnTheAirTv>(_onFetchMoreOnTheAirTv, transformer: droppable());
   }
@@ -32,33 +28,23 @@ class OnTheAirTvBloc extends Bloc<OnTheAirTvEvent, OnTheAirTvState> {
     Emitter<OnTheAirTvState> emit,
   ) async {
     emit(const OnTheAirTvState.loading());
-
     // Fetch kategori secara paralel
-
     final results = await Future.wait([getOnTheAirTv.execute(1)]);
-
     // Cek jika ada kegagalan di salah satu request
-
     final anyFailure = results.indexWhere((res) => res.isLeft());
-
     if (anyFailure != -1) {
       results[anyFailure].fold(
         (failure) => emit(OnTheAirTvState.error(failure.message)),
-
         (_) {},
       );
-
       return;
     }
 
     // Jika semua berhasil, emit state Loaded
-
     emit(
       OnTheAirTvState.loaded(
         onTheAir: results[0].getOrElse(() => []),
-
         onTheAirPage: 2,
-
         hasMoreOnTheAir: results[0].getOrElse(() => []).isNotEmpty,
       ),
     );
@@ -66,36 +52,27 @@ class OnTheAirTvBloc extends Bloc<OnTheAirTvEvent, OnTheAirTvState> {
 
   Future<void> _onRefreshTvOTA(
     RefreshTvOTA event,
-
     Emitter<OnTheAirTvState> emit,
   ) async {
     // Cukup panggil event untuk fetch awal lagi
-
     add(const FetchInitialOnTheAir());
   }
 
   Future<void> _onFetchMoreOnTheAirTv(
     FetchMoreOnTheAirTv event,
-
     Emitter<OnTheAirTvState> emit,
   ) async {
     if (state is Loaded) {
       final currentState = state as Loaded;
-
       if (!currentState.hasMoreOnTheAir) return;
-
       final result = await getOnTheAirTv.execute(currentState.onTheAirPage);
-
       result.fold(
         (failure) => emit(currentState.copyWith(minorError: failure.message)),
-
         (newTvS) {
           emit(
             currentState.copyWith(
               onTheAir: List.of(currentState.onTheAir)..addAll(newTvS),
-
               onTheAirPage: currentState.onTheAirPage + 1,
-
               hasMoreOnTheAir: newTvS.isNotEmpty,
             ),
           );
